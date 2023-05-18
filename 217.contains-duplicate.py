dd86b405c8cdf8ba9217635efcb53c13791a9364
phuckhangbui <khangpro592@gmail.com>
2023-05-18 20:03:39 +0700

    solve contains duplicate - using hashset

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+# @lc code=end
+
